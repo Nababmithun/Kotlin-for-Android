234724eea7cfae7b6b376936cf60a5b2ed1ebebb
Nababmithun <monermithun789@gmail.com>
2021-04-04 00:16:27 +0600

    if---else when---else

--- app/src/main/java/com/example/kotlinforandroid/MainActivity.kt
+++ app/src/main/java/com/example/kotlinforandroid/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kotlinforandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -29,6 +30,32 @@ class MainActivity : AppCompatActivity() {
 
         //System Two------------
         findViewById<Button>(R.id.btn_toast).setOnClickListener { Toast.makeText(this@MainActivity,"Hi Toast",Toast.LENGTH_SHORT).show() }
+
+
+
+        //if----else when---else-----------
+
+        var listener:View.OnClickListener=View.OnClickListener { view ->
+
+
+            if (view.id==R.id.btn_one) {
+                findViewById<TextView>(R.id.tv_logic).text = "Click button 1" + (view as Button).text
+            }else{
+                findViewById<TextView>(R.id.tv_logic).text="Wrong btn click"
+            }
+        }
+
+
+        var button1=findViewById<Button>(R.id.btn_one)
+        var button2=findViewById<Button>(R.id.btn_two)
+        var button3=findViewById<Button>(R.id.btn_three)
+        var button4=findViewById<Button>(R.id.btn_four)
+
+
+        button1.setOnClickListener { listener }
+        button2.setOnClickListener { listener }
+        button3.setOnClickListener { listener}
+        button4.setOnClickListener { listener }
 
 
     }
