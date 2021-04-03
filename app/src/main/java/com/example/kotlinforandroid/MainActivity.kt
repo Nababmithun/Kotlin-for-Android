5e04e21bfa685ac851d01fdbc81fa0904a602ccd
Nababmithun <monermithun789@gmail.com>
2021-04-03 23:34:11 +0600

    btn with Toast show

--- app/src/main/java/com/example/kotlinforandroid/MainActivity.kt
+++ app/src/main/java/com/example/kotlinforandroid/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,13 +12,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        //btn click with Textview Create
         val btnhello = findViewById<Button>(R.id.btn_hello)
         val tvhello =findViewById<TextView>(R.id.tv_hello)
-
 
         btnhello.setOnClickListener{
             tvhello.text="Hello from Kotlin. Enjoy simplicity!"
         }
+
+
+
+        //btn click with Toast Show
+        //System one ---------
+        val messsage:String="Hi Toast"
+        findViewById<Button>(R.id.btn_toast).setOnClickListener { Toast.makeText(this@MainActivity,messsage,Toast.LENGTH_SHORT).show() }
+
+        //System Two------------
+        findViewById<Button>(R.id.btn_toast).setOnClickListener { Toast.makeText(this@MainActivity,"Hi Toast",Toast.LENGTH_SHORT).show() }
 
 
     }
